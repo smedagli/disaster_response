4928ea74baf196486f88f7eaec632357e5c8a26f
medagli <Stefano.Medagli@rohde-schwarz.com>
2020-06-19 16:43:07 +0200

    feat: add functions to print performance of the model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -61,26 +61,27 @@ def build_model(verbose=10) -> GridSearchCV:
     pipeline = Pipeline([
         ('features', FeatureUnion([
             ('nlp_pipeline', base_pipeline),
-            ('help', TextExtractor(word_to_find='help')),
-            ('need', TextExtractor(word_to_find='need')),
-            ('sos', TextExtractor(word_to_find='sos')),
-            ('please', TextExtractor(word_to_find='please')),
+            # ('help', TextExtractor(word_to_find='help')),
+            # ('need', TextExtractor(word_to_find='need')),
+            # ('sos', TextExtractor(word_to_find='sos')),
+            # ('please', TextExtractor(word_to_find='please')),
             ('text_len', LenExtractor()),
         ], n_jobs=-1),
          ),
         ('model', MultiOutputClassifier(estimator=RandomForestClassifier(verbose=verbose))),
     ])
-    # # define parameters for GridSearchCV
-    # parameters = {'model__estimator__n_estimators': [2, 4],
-    #               'features__nlp_pipeline__tfidf__use_idf': (True, False),
-    #               'features__help__word_to_find': ['help'],
-    #               'features__need__word_to_find': ['need'],
-    #               'features__sos__word_to_find': ['sos'],
-    #               'features__please__word_to_find': ['please'],
-    #               }
-    # # create gridsearch object and return as final model pipeline
-    # model_pipeline = GridSearchCV(pipeline, param_grid=parameters, verbose=verbose)
-    model_pipeline = pipeline
+    # define parameters for GridSearchCV
+    parameters = {'model__estimator__n_estimators': [50, 100, 200, 500],
+                  'features__nlp_pipeline__vect__ngram_range': [(1, 1), (1, 2)],
+                  'features__nlp_pipeline__tfidf__use_idf': (True, False),
+                  # 'features__help__word_to_find': ['help'],
+                  # 'features__need__word_to_find': ['need'],
+                  # 'features__sos__word_to_find': ['sos'],
+                  # 'features__please__word_to_find': ['please'],
+                  }
+    # create gridsearch object and return as final model pipeline
+    model_pipeline = GridSearchCV(pipeline, param_grid=parameters, verbose=verbose)
+    # model_pipeline = pipeline
     return model_pipeline
 
 
@@ -102,16 +103,35 @@ def train(X, y, model, verbose=True):
     # output model test results
     if verbose:
         print("Evaluating performance")
-        pred_training = model.predict(X_train)
-        acc_training = accuracy(pred_training, Y_train)
-
-        pred_test = model.predict(X_test)
-        acc_test = accuracy(pred_test, Y_test)
+        acc_training = _get_performance(model, X_train, Y_train)
+        acc_test = _get_performance(model, X_test, Y_test)
         print('Model accuracy')
         print('--------------')
-        print(f"Training accuracy:\t {acc_training.mean():3.3f}")
-        print(f"Test accuracy:\t\t {acc_test.mean():3.3f}")
+        # print(f"Training accuracy:\t {acc_training.mean():3.3f}")
+        print("Training accuracy")
+        _print_performance(acc_training, verbosity='all')
+        # print(f"Test accuracy:\t\t {acc_test.mean():3.3f}")
+        print("Test accuracy")
+        _print_performance(acc_test, verbosity='all')
     return model
+
+
+def _get_performance(model, X, Y):
+    """ Returns the accuracy of the model """
+    pred = model.predict(X)
+    return accuracy(pred, Y)
+
+
+def _print_performance(accuracy: pd.Series, verbosity='mean') -> None:
+    """ Prints the accuracy value(s)
+    Args:
+        accuracy: accuracy of the predictor as a Series
+        verbosity: if 'mean' will print only the mean value, otherwise, will print to screen each label's accuracy
+    """
+    if verbosity == 'mean':
+        print("{:3.3f}".format(accuracy.mean()))
+    else:
+        print(accuracy.round(3))
 
 
 def export_model(model, pickle_file=paths.model_pickle_file) -> None:
@@ -149,9 +169,14 @@ def main(database_file=paths.sql_path, pickle_file=paths.model_pickle_file, writ
         export_model(model, pickle_file)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        database_file, pickle_file = sys.argv[1 :]
-        main(database_file, pickle_file)
-    else:
-        print("Please specify the database path and the output path")
+if __name__ == "__main__":
+    X, Y = load_data(paths.sql_path)
+    model = build_model()
+    train(X, Y, model)
+#
+# if __name__ == '__main__':
+#     if len(sys.argv) == 3:
+#         database_file, pickle_file = sys.argv[1 :]
+#         main(database_file, pickle_file)
+#     else:
+#         print("Please specify the database path and the output path")
